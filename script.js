2381c43e34dfb485c91bcd9686d14add052ae9e3
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-11-17 19:02:41 +0100

    Cambios en JS

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ const myButton = document.getElementById("submit").addEventListener("click", () 
   result.innerHTML = myOutput;
   myInput.value = ""; // cada vez que le damos a añadir, el valor del input se resetea
 });
-
-// Creamos un Event Listener en el botón.
-// Extraemos el valor del input y se lo agregamos a una variable vacia.
-// Seleccionamos donde queremos que aparezca el texto y con innerHTML le asignamos la variable con el valor guardado.
